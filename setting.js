15397898a2c866b6298a61d6e9a54906fd3ebe13
EQuimper <quimperemanuel@gmail.com>
2016-10-21 21:09:14 -0400

    Setup routes redux

---
+++ setting.js
@@ -0,0 +1,3 @@
+export default {
+  ROOT_URL: 'https://equimper-torrent-api.herokuapp.com/'
+};
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
