96244cae148e4ef48e53409a4da06d1a38c52f51
EQuimper <quimperemanuel@gmail.com>
2016-10-21 21:23:05 -0400

    Setup material

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { NavBar } from './modules';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = ({ children }) => (
+  <MuiThemeProvider>
+    <div>
+      <NavBar />
+      {children}
+    </div>
+  </MuiThemeProvider>
+);
 
 export default App;
---
+++ src/modules/layout/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const NavBar = () => (
+  <AppBar
+    title="ShowMeTheTorrent"
+  />
+);
+
+export default NavBar;
